99f3a8a7af7e7826002ed33f80304ff9d5468a65
Mishco <michal.slovik@tutanota.com>
2025-03-07 13:45:55 +0100

    Update TicTacToeChecker

--- src/main/java/kata/kyu5/TicTacToeChecker.java
+++ src/main/java/kata/kyu5/TicTacToeChecker.java
@@ -2,49 +2,52 @@ package kata.kyu5;
 
 public class TicTacToeChecker {
 
+    private TicTacToeChecker() {
+    }
+
     public static int isSolved(int[][] board) {
         // check two diagonals
-        if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
+        if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[0][0] != 0) {
             return board[0][0];
         }
 
         // check two diagonals
-        if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) {
+        if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && board[0][2] != 0) {
             return board[0][2];
         }
 
-        int total_sum_row = 0;
-        int total_sum_col = 0;
+        int totalSumRow = 0;
+        int totalSumCol = 0;
         for (int row = 0; row < board.length; row++) {
 
             // check three columns
-            int first_item = board[row][0];
-            int second_item = board[row][1];
-            int third_item = board[row][2];
-            total_sum_row += first_item + second_item + third_item;
+            int firstItem = board[row][0];
+            int secondItem = board[row][1];
+            int thirdItem = board[row][2];
+            totalSumRow += firstItem + secondItem + thirdItem;
 
-            if ((first_item == second_item) && (second_item == third_item) && (third_item != 0)) {
-                return first_item;
+            if ((firstItem == secondItem) && (secondItem == thirdItem) && (thirdItem != 0)) {
+                return firstItem;
             }
 
             // check three rows
-            first_item = board[0][row];
-            second_item = board[1][row];
-            third_item = board[2][row];
-            total_sum_col += first_item + second_item + third_item;
+            firstItem = board[0][row];
+            secondItem = board[1][row];
+            thirdItem = board[2][row];
+            totalSumCol += firstItem + secondItem + thirdItem;
 
-            if ((first_item == second_item) && (second_item == third_item) && (third_item != 0)) {
-                return first_item;
+            if ((firstItem == secondItem) && (secondItem == thirdItem) && (thirdItem != 0)) {
+                return firstItem;
             }
         }
 
         // Control if board is not yet finished -1
-        if (total_sum_row <= 7 || total_sum_col <= 7) {
+        if (totalSumRow <= 7 || totalSumCol <= 7) {
             return -1;
         }
 
         // Control if board is not yet finished -1
-        if (total_sum_row <= 12 || total_sum_col <= 12) {
+        if (totalSumRow <= 12 || totalSumCol <= 12) {
             return -1;
         }
 
